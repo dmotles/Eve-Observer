41216a3a0c203901615c8637b6b96449b1480d68
Daniel Motles <seltom.dan@gmail.com>
2013-07-29 20:43:08 -0400

    web.js for serving content

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var eve = require("./eve");
+var app = express();
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/api/igb', fuction(req, res) {
+//   res.send(eve.getIGBHeaders(req));
+// });
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
